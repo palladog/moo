d2cd387d92331edf49a9d32950334c02c4ede375
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-20 12:18:14 +0200

    corresponding router fix in app + fetching data from ghibli

--- moo/src/App.js
+++ moo/src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Nav from './components/nav/nav'
+import Header from './components/header/header';
+import Home from './components/home/home';
+import Watched from './components/watched/watched';
+import NotWatched from './components/notWatched/notWatched';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 function App() {
+  const [state, setState] = useState({ghibli: null})
+
+  const getGhibliData = () => {
+    fetch('https://ghibliapi.herokuapp.com/films') // Returns all films
+    .then(response=>response.json())
+    .then(response => {
+      setState({...state,
+        ghibli: response
+      })
+    })
+  }
+
+  useEffect(() =>{
+    getGhibliData()
+  }, [])
+  
+
   return (
     <div className="App">
-      <Nav />
+      <Router>
+        <Header/>
+          <Nav />
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/watched">
+              <Watched />
+            </Route>
+            <Route path="/not-watched">
+              <NotWatched />
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
