c2d32e2565876c36b2dc3a2c706607a133ab4201
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-20 11:21:02 +0200

    nav updated

--- moo/src/components/nav/nav.js
+++ moo/src/components/nav/nav.js
@@ -1,40 +1,21 @@
-import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom"
+import React from 'react';
+import './nav.css';
+import { Link } from "react-router-dom";
 
 export default () => {
     return(
-        <Router>
-        <div>
           <nav>
-            <ul>
-              <li>
+            <ul className="menu">
+              <li className="menu-item">
                 <Link to="/">All</Link>
               </li>
-              <li>
+              <li className="menu-item">
                 <Link to="/watched">Watched</Link>
               </li>
-              <li>
-                <Link to="/not-watched">Not Watched</Link>
+              <li className="menu-item">
+                <Link to="/NotWatched">Not Watched</Link>
               </li>
             </ul>
-          </nav>
-          <Switch>
-            <Route path="/">
-              {/* <Home /> */}
-            </Route>
-            <Route path="/watched">
-              {/* <Watched /> */}
-            </Route>
-            <Route path="/not-watched">
-              {/* <Not-watched /> */}
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+          </nav>      
     )
-}
+};
