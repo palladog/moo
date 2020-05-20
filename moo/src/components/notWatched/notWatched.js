32c154000395b8c0ad8117408be9e97bafe84e05
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-20 15:30:31 +0200

    notwatched filter added

--- moo/src/components/notWatched/notWatched.js
+++ moo/src/components/notWatched/notWatched.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import MovieContainer from '../movies/movieContainer/movieContainer';
 
-export default () => {
+export default (props) => {
+    let notWatchedMovies = props.movies.filter(movie => movie.rating === null)
+    if(notWatchedMovies.length === 0){
+        return("DU HAR DÅ SETT ALLA FILMER") //placeholder
+    }
     return (
         <React.Fragment>
             <h2 className="hidden">Not Watched</h2>
             <p>Not watched...</p>
+            <MovieContainer movies={notWatchedMovies}/>
         </React.Fragment>
         
     )
