f571f34e4109ccc0f48107fd0aa6dabd878daf1f
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-20 10:31:21 +0200

    added router framework

--- moo/src/App.js
+++ moo/src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+        <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All</Link>
+            </li>
+            <li>
+              <Link to="/watched">Watched</Link>
+            </li>
+            <li>
+              <Link to="/not-watched">Not Watched</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/">
+            {/* <Home /> */}
+          </Route>
+          <Route path="/watched">
+            {/* <Watched /> */}
+          </Route>
+          <Route path="/not-watched">
+            {/* <Not-watched /> */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
       
     </div>
   );
