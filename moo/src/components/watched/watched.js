1cda76eed49ca799419bf0f3cce474c0ce41be05
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-20 15:30:18 +0200

    watched filter added

--- moo/src/components/watched/watched.js
+++ moo/src/components/watched/watched.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import MovieContainer from '../movies/movieContainer/movieContainer';
 
-export default () => {
+export default (props) => {
+    let watchedMovies = props.movies.filter(movie => movie.rating !== null)
+    if(watchedMovies.length === 0){
+        return("KOLLA PÅ NÅGON FILM OCH BETYGSÄTT DEN DÅ") //placeholder
+    }
     return (
         <React.Fragment>
             <h2 className="hidden">Watched</h2>
             <p>WATCHED!</p>
+            <MovieContainer movies={watchedMovies}/>
         </React.Fragment>       
     )
 }
