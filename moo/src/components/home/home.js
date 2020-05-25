d4816606134cacc03297bcce315b1339f4baa240
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-25 22:35:49 +0200

    added a rating comp

--- moo/src/components/home/home.js
+++ moo/src/components/home/home.js
@@ -5,7 +5,6 @@ export default (props) => {
     return (
         <React.Fragment>
             <h2 className="hidden">All movies</h2> 
-            <p>home</p>
             <MovieContainer movies={props.movies}/>
         </React.Fragment>
     )
