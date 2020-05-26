dafef3bdb17a1cf3b1d119456ccf623e72dd4ebf
OmarAlsaif <omaralsaif1996@gmail.com>
2020-05-26 10:53:00 +0200

    active menu fixad

--- moo/src/components/nav/nav.js
+++ moo/src/components/nav/nav.js
@@ -7,14 +7,14 @@ export default () => {
           <nav>
             <ul className="menu">
               <li className="menu-item">
-                <NavLink to="/" activeClassName="active"><span className="title">Home</span></NavLink>
+                <NavLink exact to="/" activeClassName="active"><span className="title">Home</span></NavLink>
               </li>
               <li className="menu-item">
-                <NavLink to="/watched" activeClassName="active"><span className="title">Watched</span></NavLink>
+                <NavLink exact to="/watched" activeClassName="active"><span className="title">Watched</span></NavLink>
               </li>
               
               <li className="menu-item">
-                <NavLink to="/not-watched" activeClassName="active"><span className="title">Not Watched</span></NavLink>
+                <NavLink exact to="/not-watched" activeClassName="active"><span className="title">Not Watched</span></NavLink>
               </li>
             </ul>
           </nav>      
