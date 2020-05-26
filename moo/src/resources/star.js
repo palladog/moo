f03ffa88c858efdbde4877fe8b3c7863fc7b4011
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-05-26 09:34:29 +0200

    fixed rating func

--- moo/src/resources/star.js
+++ moo/src/resources/star.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 export default (props) => {
-    const {color, width, height } = props;
+    const { color } = props;
 
     return(
         <svg 
+        className='rating-icons'
+        alt='rating-star'
         onMouseEnter={() => props.setHover(props.ratingVal)}
         onMouseLeave={() => props.setHover(null)}
-        width={'6%'} viewBox="0 0 53.867 53.867">
+        viewBox="0 0 53.867 53.867">
             <polygon points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798 10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 " fill={color ? color : "#333"} />
         </svg>
 )
